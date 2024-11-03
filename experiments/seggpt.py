57c601fb7a34f200c73ea7d3b93fb888406f0b1d
Tidiane <camaret.tidiane@gmail.com>
2024-11-03 06:34:10 +0100

    Add SegGPT inference script and sample label images

---
+++ experiments/seggpt.py
@@ -0,0 +1,7 @@
+""""
+https://github.com/baaivision/Painter/blob/main/SegGPT/SegGPT_inference/README.md
+"""
+
+"""
+python seggpt_inference.py --input_image examples/brain_imgs/slice_30_img.jpg --prompt_image examples/brain_imgs/slice_31_img.jpg --prompt_target examples/brain_imgs/slice_31_labels.png --output_dir ./ --device cpu
+"""
